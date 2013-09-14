e304da9abe0190a115893e4996bff62c227b523f
Matthias Klein <matthias@klein.pw>
2013-09-14 07:45:52 +0200

    Update tests

--- test/client.js
+++ test/client.js
@@ -1,11 +1,25 @@
 /*global trajan,trajangw,expect,listen*/
 
+function connect(serviceId, gameId, clientId, callback) {
+  var server = listen(function(port) {
+    var gateway = trajangw();
+    gateway.addService(serviceId, 'localhost:' + port);
+    var route = gateway.route(serviceId, gameId, clientId);
+    if (callback) callback(route);
+  });
+  return server;
+}
+
 describe('client', function() {
+  it('should have an id', function(done) {
+    var server = connect('myservice', 'mygame', 'myclient');
+    server.on('connection', function(client, gameId) {
+      expect(client.id).to.equal('myclient');
+      done();
+    });
+  });
   it('should receive actions', function(done) {
-    var server = listen(function(port) {
-      var gateway = trajangw();
-      gateway.addService('test', 'localhost:' + port);
-      var route = gateway.route('test', 'test', 'test');
+    var server = connect('s', 'g', 'c', function(route) {
       server.on('connection', function(client, gameId) {
         route.send('myaction');
         client.on('action', function(str) {
@@ -16,10 +30,7 @@ describe('client', function() {
     });
   });
   it('should receive disconnect', function(done) {
-    var server = listen(function(port) {
-      var gateway = trajangw();
-      gateway.addService('test', 'localhost:' + port);
-      var route = gateway.route('test', 'test', 'test');
+    var server = connect('s', 'g', 'c', function(route) {
       server.on('connection', function(client, gameId) {
         route.disconnect();
         client.on('disconnect', function() {
